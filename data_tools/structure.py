45a1e2cf8fb038290d431393d4e485d48636d5e7
Jackson L. Cole <jackson.lane.cole@gmail.com>
2018-02-14 11:32:38 -0600

    Created general purpose directory structrue class

--- data_tools/structure.py
+++ data_tools/structure.py
@@ -2,55 +2,29 @@ from os import path, mkdir
 
 
 class structure:
-    def __init__(self, structure_file):
-        '''Constructor for this class'''
-        self.structure_elements = \
-                self.read_structure_file(structure_file)
+    def __init__(self, path_list):
+        '''
+        Constructor for this class:
+            This class takes a list as an argument that contains the
+            directory structure that should be created.
+            The path tuple should have the form
+            path_tuple = ('root', 'child_1', 'child_2', ...)
+        '''
+        self.paths = self.get_structure_strings(path_list)
 
 
-    def read_structure_file(self, structure_file):
-        structure_elements = []
-        with open(structure_file, 'r') as f:
-            for line in f:
-                line_list = line.rstrip('\n').split(' ')
-                structure_elements.append(line_list)
+    def get_structure_strings(self, path_list):
+        paths = []
+        for i in range(len(path_list)):
+            paths.append('./' + '/'.join(j for j in path_list[:i+1]))
 
-        return structure_elements
-
-    '''
-    def create(self, depth_id, root_name, dir_name):
-        if path.isdir('./{}'.format(root_name)) == False:
-            mkdir('./{}'.format(root_name))
+        return paths
 
 
-    def is_root(self, depth_id):
-        if (depth_id == 'r'):
-            return True
-        else:
-            return False
-
-    def how_deep(self, line):
-        depth_id = line.split(' ')[0]
-        if (depth_id == 'r'):
-            return
-        name = line.split(' ')[1]
-
-        return is_root, depth, variable
-
-
-
-    def check_path(self, depth, root):
-
-
-
-
-    def create_root(self, root_name):
-        if path.isdir('./{}'.format(root_name)) == False:
-            mkdir('./{}'.format(root_name))
-
-    '''
-def main():
-    galaxies = structure('outputstructure.txt')
-    print(galaxies.structure_elements)
+    def create(self, paths):
+        for i, path in enumerate(paths):
+            if path.isdir(path) == False:
+                for j in paths[i:]:
+                    mkdir(j)
 
 main()
