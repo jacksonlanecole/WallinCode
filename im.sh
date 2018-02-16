538039154ad479d0d12c0f5e3debca96304c90ce
Mr. Matthew Benjamin Ogden <mbo2d@Herschel.csd.mtsu.edu>
2018-02-16 17:52:53 -0600

    altering script

--- im.sh
+++ im.sh
@@ -1,2 +1,2 @@
-g++ -Wall image_creator/main.cpp -o image.out -ggdb `pkg-config --cflags --libs opencv`
+g++ -Wall image_creator/im.cpp image_creator/Galaxy_Class.cpp -o image.out -ggdb `pkg-config --cflags --libs opencv`
 ./image.out image_creator/im.config image_creator/im.param
