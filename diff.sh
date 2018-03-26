ae093aafcfa5943f3c66f7170c63c3b2e777a14e
Jackson Lane Cole <jlc2de@Herschel.csd.mtsu.edu>
2018-03-26 14:22:45 -0500

    Added target info as third argument.

--- diff.sh
+++ diff.sh
@@ -1,2 +1,2 @@
 g++ -ggdb difference_code/diff_v2.cpp -o diff.out `pkg-config --cflags --libs opencv`
-./diff.out output/587722984435351614/ output/587722984435351614/target_images/587722984435351614.calibrated.png
+./diff.out output/587722984435351614/ targets/587722984435351614/587722984435351614.calibrated.png targets/587722984435351614/587722984435351614.info.txt
